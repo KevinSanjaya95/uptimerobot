5d27f159b046c9671aff44f87996adb237fef08a
KevinSanjaya95 <57531770+KevinSanjaya95@users.noreply.github.com>
2020-09-26 01:49:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://glitch.com/~rain-pinnate-cadmium"]
+const urls = ["https://glitch.com/~large-narrow-cabinet"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
